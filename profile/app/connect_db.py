4f9a2872d8a0f5e989fb270ad7292a36376f1ec9
Administrator <admin@example.com>
2021-02-13 17:32:24 +0100

    profile updated, methode update a débugger

---
+++ profile/app/connect_db.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import jsonify, request
+
+import sqlite3
+
+''' Se connecter à la base '''
+def connection_db():
+    return sqlite3.connect('profile_base.db')
+
+def close_db(db):
+    db.commit()
+    db.close()
+
+def execute_query(db, query, params=()):
+    cursor = db.cursor()
+    if params :
+        cursor.execute(query, params)
+    else :
+        cursor.execute(query)
+    
+    return cursor
+
+def create_db():
+    try:
+        conn = sqlite3.connect('profile_base.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+    CREATE TABLE users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name TEXT,
+        age INTEGER,
+        email TEXT
+    )
+    """)
+        conn.commit()
+    except sqlite3.OperationalError:
+        print('Erreur la table existe déjà')
+    except Exception as e:
+        print("Erreur")
+        conn.rollback()
+        # raise e
+    finally:
+        conn.close()
